4f007f8cc1b1397b27d83b4891a73aa2ec052758
Matt Neary <neary.matt@gmail.com>
2014-03-11 16:57:01 -0400

    scripts to help in running proxy and nvm

---
+++ chrome.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+/Applications/Google\ Chrome.app/Contents/MacOS/Google\ Chrome --proxy-server=localhost:8080 --ignore-certificate-errors
+
---
+++ run.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+source /etc/bashrc
+source ~/.bash_profile
+nvm use default
+node index.js
+
