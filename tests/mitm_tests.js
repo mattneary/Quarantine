467b62038a96a916ab12dc3f9bf6443c90e6566c
Matt Neary <neary.matt@gmail.com>
2014-03-21 11:17:15 -0400

    core functions for testing the proxy

--- tests/mitm_tests.js
+++ tests/mitm_tests.js
@@ -3,31 +3,47 @@ var mitm = require(__dirname + '/../src/mitm'),
     assert = require(__dirname + '/test.js'),
     assert_async = require(__dirname + '/async_test.js');
 
-assert_async("Verify mitm-proxy for basic http GET.", function(cb) {
-  var http = require('http');
-  var dest_server = http.createServer(function(req, res) {
-    res.write("Success.");
-    res.end();
-  }).listen(80);
+var start_servers = function(dest, port) {
+  var dest_server = http.createServer(dest).listen(80);
   var proxy_server = http.createServer(mitm(
     http, function(req, res, parsed, cb) { cb(); }
-  )).listen(8080);
-
-  var req = http.request({
-    method: 'GET',
+  )).listen(port);
+  return [dest_server, proxy_server];
+};
+var tunnel_request = function(method, port, url, cb) {
+  return http.request({
+    method: method,
     host: 'localhost',
-    port: '8080',
-    path: 'http://localhost'
+    port: port,
+    path: url
   }, function(res) {
     var b = [];
     res.on("data", function(c) { b.push(c); });
     res.on("end", function() {
-      dest_server.close();
-      proxy_server.close();
-      cb(b.join('') == 'Success.');
+      cb(b.join(''));
     });
   });
+};
+var tunnel_get = function(port, url, cb) {
+  tunnel_request('GET', port, url, cb).end();
+};
+var tunnel_post = function(port, url, body, cb) {
+  var req = tunnel_request('POST', port, url, cb);
+  req.write(body);
   req.end();
+};
+
+assert_async("Verify mitm-proxy for basic http GET.", function(cb) {
+  var http = require('http');
+  var servers = start_servers(function(req, res) {
+    res.write("Success.");
+    res.end();
+  }, 8080);
+
+  tunnel_get(8080, "http://localhost", function(body) {
+    servers.map(function(server){ server.close(); });
+    cb(body == 'Success.');
+  });
 });
 
 
