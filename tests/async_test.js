fd1a7312c3503ce78d982eb4b50ccbc6e2872637
Matt Neary <neary.matt@gmail.com>
2014-03-21 11:04:56 -0400

    timing for async tests

--- tests/async_test.js
+++ tests/async_test.js
@@ -1,7 +1,22 @@
 var assert = require(__dirname + '/test.js');
+var AsyncTimer = function() {
+  var startTime;
+  this.start = function() {};
+  this.startAsync = function() {
+    startTime = new Date().getTime();
+  };
+  this.end = function() {
+    return new Date().getTime() - startTime;
+  };
+};
 module.exports = function(desc, fn) {
+  var timer = new AsyncTimer();
+  timer.startAsync();
   fn(function(result) {
-    assert(desc, function() { return result; });
+    assert.call({
+      timer: timer, 
+      verbose: false
+    }, desc, function() { return result; });
   });
 };
 
--- tests/test.js
+++ tests/test.js
@@ -7,17 +7,22 @@ var Timer = function() {
     return new Date().getTime() - startTime;
   };
 };
-var timer = new Timer(), verbose = false;
+var timeSpent = 0;
+var opts = { timer: new Timer(), verbose: false };
 process.on('exit', function() {
-  console.log("\nAll tests passed. Finishing.");
+  console.log("\nAll tests passed in "+timeSpent+" ms. Finishing.");
 });
-module.exports = function(assertion, fn, verbose) {
-  timer.start();
+module.exports = function(assertion, fn) {
+  this.timer = this.timer === undefined ? opts.timer : this.timer;
+  this.verbose = this.verbose === undefined ? opts.verbose : this.verbose;
+  this.timer.start();
   var resp = fn();
   if( resp !== true ) {
     throw new Error("\x1B[31mFailed\x1B[39m `"+assertion+"`!", JSON.stringify(resp));
   } else {
-    if( verbose ) console.log("\x1B[32mPassed\x1B[39m `"+assertion+"` in "+timer.end()+" ms.");
+    var duration = this.timer.end();
+    if( this.verbose ) console.log("\x1B[32mPassed\x1B[39m `"+assertion+"` in "+duration+" ms.");
+    timeSpent += duration;
     process.stdout.write(".");
   }
 };
